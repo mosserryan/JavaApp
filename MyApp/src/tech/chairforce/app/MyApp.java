69dce1d82d4d82f5b94bd4e6695578eb7f916750
mosse <mosse@DESKTOP-QJB01P7>
2021-12-31 18:05:09 -0800

    Testing commit.

---
+++ MyApp/src/tech/chairforce/app/MyApp.java
@@ -0,0 +1,15 @@
+package tech.chairforce.app;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		sayGreeting("Hello World!");
+		System.out.println(0);
+
+	}
+
+	private static void sayGreeting(String word) {
+		System.out.println(word);
+	}
+
+}
