3bccf02b538f33492a413c5d0b0afdbba5749811
mosse <mosse@DESKTOP-QJB01P7>
2022-01-01 17:48:12 -0800

    Getting gitty with github and eclipse.

--- MyApp/src/tech/chairforce/app/MyApp.java
+++ MyApp/src/tech/chairforce/app/MyApp.java
@@ -3,13 +3,17 @@ package tech.chairforce.app;
 public class MyApp {
 
 	public static void main(String[] args) {
-		sayGreeting("Hello World!");
+		sayGreeting(myName("Ryan"));
 		System.out.println(0);
 
 	}
 
 	private static void sayGreeting(String word) {
-		System.out.println(word);
+		System.out.println("Hello " + word);
+	}
+	
+	private static String myName(String name) {
+		return name;
 	}
 
 }
