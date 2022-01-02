fb17e931f98520ec20a62b169fb73094f330510d
mosserryan <mosserryan@yahoo.com>
2022-01-02 15:26:47 -0800

    Commenting code in eclipse, more note clean up.

--- MyApp/src/tech/chairforce/app/MyApp.java
+++ MyApp/src/tech/chairforce/app/MyApp.java
@@ -2,21 +2,18 @@ package tech.chairforce.app;
 
 public class MyApp {
 
-	public static void main(String[] args) {
-		sayGreeting(myName("Ryan"));
-		System.out.println(0);
-		System.out.println("Hello World!");
-		
-
-	}
-
-	private static void sayGreeting(String word) {
-		System.out.println("Hello " + word);
-	}
-	
-	private static String myName(String name) {
-		return name;
-	}
+	/*
+	 * public static void main(String[] args) { sayGreeting(myName("Ryan"));
+	 * System.out.println(0); System.out.println("Hello World!");
+	 * 
+	 * 
+	 * }
+	 * 
+	 * private static void sayGreeting(String word) { System.out.println("Hello " +
+	 * word); }
+	 * 
+	 * private static String myName(String name) { return name; }
+	 */
 
 	
 	
