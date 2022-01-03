696d93d71eb9beaf26001413ab68daff0b83d1b1
mosserryan <mosserryan@yahoo.com>
2022-01-02 16:03:12 -0800

    Switching variables method.

--- MyApp/src/tech/chairforce/app/MyApp.java
+++ MyApp/src/tech/chairforce/app/MyApp.java
@@ -1,23 +1,34 @@
 package tech.chairforce.app;
 
 public class MyApp {
-
-	/*
-	 * public static void main(String[] args) { sayGreeting(myName("Ryan"));
-	 * System.out.println(0); System.out.println("Hello World!");
-	 * 
-	 * 
-	 * }
-	 * 
-	 * private static void sayGreeting(String word) { System.out.println("Hello " +
-	 * word); }
-	 * 
-	 * private static String myName(String name) { return name; }
-	 */
-
 	
+	public static void main(String[] args) {
+		
+		String x = "Water";
+		String y = "Juice";
+		
+		System.out.println("variable x is: " + x);
+		System.out.println("variable y is: " + y);
+		
+		switchVars(x,y);	
+		
+	}
 	
-	
+	private static void switchVars(String x, String y) {
+		
+		System.out.println("Switching values...");
+		
+		// Switching variable value by creating a "temp" holder variable.
+		String temp;
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("Switched!");
+		System.out.println("variable x is: " + x);
+		System.out.println("variable y is: " + y);
+		
+	};
 	
 	
 }
