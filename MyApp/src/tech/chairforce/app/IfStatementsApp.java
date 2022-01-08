0faf7da6046ae4ba41059e32e343561855fc8dce
mosserryan <mosserryan@yahoo.com>
2022-01-08 12:47:02 -0800

    Using and (&&) and or (||) operators with my if statements.

--- MyApp/src/tech/chairforce/app/IfStatementsApp.java
+++ MyApp/src/tech/chairforce/app/IfStatementsApp.java
@@ -6,16 +6,43 @@ public class IfStatementsApp {
 
 	public static void main(String[] args ) {
 				
+		canDrive();
+		
+	}
+	
+	public static void gigaChad() {
 		String input = JOptionPane.showInputDialog("What is Giga Chad Daddy's favorite color?");
 		System.out.println(input);
 		
-		if(input.equals("blue")) {
+		if(input.equals("blue") || input.equals("Blue")) {
 			
 			JOptionPane.showMessageDialog(null, "You said blue! You're right! Giga Chad Daddy is pleased!");
 			
 		} else {
 			
 			JOptionPane.showMessageDialog(null, "You said " + input + ", you're Wrong! You have disappointed Giga Chad Daddy :(");
+			
+		}
+	}
+	
+	public static void canDrive() {
+		int age = 10;
+		boolean hasLicense = true;
+		
+		if(age >= 16 && hasLicense == true) {
+			
+			System.out.println("Age: " + age + ", Licensed: " + hasLicense);
+			JOptionPane.showMessageDialog(null, "This guy can drive!");
+			
+		} else if (age >= 16 && hasLicense == false) {
+			
+			System.out.println("Age: " + age + ", Licensed: " + hasLicense);
+			JOptionPane.showMessageDialog(null, "This melvin is old enough, but no license. They can't drive!");
+			
+		} else {
+			
+			System.out.println("Age: " + age + ", Licensed: " + hasLicense);
+			JOptionPane.showMessageDialog(null, "This guy barely left the craddle. You got some aging to do!");
 			
 		}
 		
