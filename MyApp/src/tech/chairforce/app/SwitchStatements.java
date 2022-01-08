47c0ec2496f81cb4f526d055bd95075c154616b9
mosserryan <mosserryan@yahoo.com>
2022-01-07 17:47:14 -0800

    Switch statements completed, negative findings.

--- MyApp/src/tech/chairforce/app/SwitchStatements.java
+++ MyApp/src/tech/chairforce/app/SwitchStatements.java
@@ -1,5 +1,31 @@
 package tech.chairforce.app;
 
+import javax.swing.JOptionPane;
+
 public class SwitchStatements {
+	
+	public static void main(String[] args) {
+		
+		String day = JOptionPane.showInputDialog("What day is it?");
+		
+		switch(day) {
+		case "Sunday": System.out.println("It is Sunday!");
+		break;
+		case "Monday": System.out.println("It is Monday!");
+		break;
+		case "Tuesday": System.out.println("It is Tuesday!");
+		break;
+		case "Wednesday": System.out.println("It is Wednesday!");
+		break;
+		case "Thursday": System.out.println("It is Thursday!");
+		break;
+		case "Friday": System.out.println("It is Friday!");
+		break;
+		case "Saturday": System.out.println("It is Saturday!");
+		break;
+		default: System.out.println("That is not a day you Melvin!");
+		}
+		
+	}
 	
 }
