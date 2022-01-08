a625c295165d4b95857b7b98d8cefc9f33f9a813
mosserryan <mosserryan@yahoo.com>
2022-01-07 16:53:28 -0800

    Random numbers completed, int, double, and booleans.

--- MyApp/src/tech/chairforce/app/RandomNumbers.java
+++ MyApp/src/tech/chairforce/app/RandomNumbers.java
@@ -1,5 +1,20 @@
 package tech.chairforce.app;
+import java.util.Random;
 
 public class RandomNumbers {
 
+	public static void main(String[] args ) {
+		
+		Random random = new Random();
+		
+		int x = random.nextInt(10);
+		double y = random.nextDouble(10);
+		boolean z = random.nextBoolean();
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+	}
+	
 }
