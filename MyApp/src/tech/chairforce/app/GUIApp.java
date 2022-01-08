8aea48bc7e501ac4eabe6fd73805d9a56ea05329
mosserryan <mosserryan@yahoo.com>
2022-01-07 16:37:55 -0800

    Utilizing some GUI interfacing to add input capability into my application.

--- MyApp/src/tech/chairforce/app/GUIApp.java
+++ MyApp/src/tech/chairforce/app/GUIApp.java
@@ -1,5 +1,40 @@
 package tech.chairforce.app;
 
+import javax.swing.JOptionPane;
+
 public class GUIApp {
 
+	public static void main(String[] args) {
+		
+		mathStuff();
+		
+		
+	}
+	
+	public static void mathStuff() {
+		
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Enter x value: "));
+		JOptionPane.showMessageDialog(null, "x = " + x);
+		
+		int y = Integer.parseInt(JOptionPane.showInputDialog("Enter y value: "));
+		JOptionPane.showMessageDialog(null, "y = " + y);
+		
+		double z = Math.sqrt((x*x)+(y*y));
+		
+		JOptionPane.showMessageDialog(null, "The hypotenuse is: " + z);
+	}
+	
+	
+	public static void userInfo() {
+		String name = JOptionPane.showInputDialog("Enter your name.");
+		JOptionPane.showMessageDialog(null, name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age."));
+		JOptionPane.showMessageDialog(null, age);
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height."));
+		JOptionPane.showMessageDialog(null, height);
+	}
+	
+	
 }
