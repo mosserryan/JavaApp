1455d6ecfb659e0e77a52bed418e2e2e83d569f1
mosserryan <mosserryan@yahoo.com>
2022-01-09 14:51:44 -0800

    For loops, and nested for loops.

--- MyApp/src/tech/chairforce/app/ForLoop.java
+++ MyApp/src/tech/chairforce/app/ForLoop.java
@@ -1,11 +1,42 @@
 package tech.chairforce.app;
+import java.util.Scanner;
 
 public class ForLoop {
 	
 	public static void main(String[] args) {
 		
-		for(int i = 0; i <= 10; i++) {
-			System.out.println(i);
+		nestLoop();		
+		/*
+		 * for(int i = 10; i >= 0; i--) { System.out.println(i); }
+		 * 
+		 * System.out.println("Happy New Year!");
+		 */
+		
+	}
+	
+	public static void nestLoop () {
+		Scanner scanner = new Scanner(System.in);
+		int rows;
+		int columns;
+		String symbol;
+		
+		System.out.println("Enter # of rows: ");
+		rows = scanner.nextInt();
+		System.out.println("Enter # of columns: ");
+		columns = scanner.nextInt();
+		System.out.println("Enter sysmbol ");
+		symbol = scanner.next();
+		
+		for(int i = 1; i < rows; i++) {
+			
+			System.out.println();
+			
+			for(int j = 1; j < columns; j++) {
+				
+				System.out.print(symbol);
+				
+			}
+			
 		}
 		
 	}
