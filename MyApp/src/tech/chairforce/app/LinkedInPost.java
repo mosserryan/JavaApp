ed2ea1379319b2465d2663e993f454585f3eff5f
mosserryan <mosser.ryan.a@gmail.com>
2022-01-11 18:20:10 -0800

    For the memes!

---
+++ MyApp/src/tech/chairforce/app/LinkedInPost.java
@@ -0,0 +1,15 @@
+package tech.chairforce.app;
+
+public class LinkedInPost {
+
+	public static void main(String[] args) {
+	
+		post();
+		
+	}
+	
+	public static void post() {
+		System.out.println("Generic line");
+	}
+	
+}
