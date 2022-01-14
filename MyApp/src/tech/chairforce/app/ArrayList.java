b1bb9ed4ced38a1d4400179965946727f99959a3
mosserryan <mosserryan@yahoo.com>
2022-01-13 16:27:49 -0800

    ArrayList practice.

---
+++ MyApp/src/tech/chairforce/app/ArrayList.java
@@ -0,0 +1,13 @@
+package tech.chairforce.app;
+
+public class ArrayList {
+	
+	// ArrayList: are a resizable array. Meaning, elements can be added and removed after the compilation phase. But only store Reference data types.
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
