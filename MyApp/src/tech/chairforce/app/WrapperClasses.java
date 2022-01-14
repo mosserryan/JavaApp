a661ca24c2d23364cd66f85e47b8cd0774e7ac23
mosserryan <mosserryan@yahoo.com>
2022-01-13 16:17:03 -0800

    Wrapper classes notes and examples.

---
+++ MyApp/src/tech/chairforce/app/WrapperClasses.java
@@ -0,0 +1,26 @@
+package tech.chairforce.app;
+
+public class WrapperClasses {
+	
+	// Wrapper Class: Provides a way to use primitive data types as reference data types.
+	//				  Reference data types contain useful methods.
+	//				  Can be used with collections (example. ArrayList)
+
+	// [Primitive]		 [Wrapper]
+	//-----------------------------
+	// boolean			Boolean
+	// char				Character
+	// int				Integer
+	// double			Double
+	
+	// autoboxing = the automatic conversion that the Java compiler makes between the primitive types and their corresponding object wrapper classes.
+	// unboxing = The reverse of autoboxing. Automatic conversion of wrapper class to primitive.
+	
+	
+	// Wrapper Classes use Capital letters.
+	Boolean a = true;
+	Character b = '@';
+	Integer c = 123;
+	Double d = 3.14;
+	String e = "Ryan";
+} 
