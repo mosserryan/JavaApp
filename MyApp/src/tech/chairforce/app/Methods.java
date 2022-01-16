2131d59235c65593ffba426d10ed5a3b5e8df506
mosserryan <mosserryan@yahoo.com>
2022-01-16 14:50:11 -0800

    Methods, finished overload Methods created.

--- MyApp/src/tech/chairforce/app/Methods.java
+++ MyApp/src/tech/chairforce/app/Methods.java
@@ -1,4 +1,5 @@
 package tech.chairforce.app;
+import javax.swing.JOptionPane;
 
 public class Methods {
 
@@ -6,6 +7,34 @@ public class Methods {
 		
 		System.out.println("Main method. Always runs first.");
 		
+		String name = "Ryan";
+		
+		greeting(name);
+		
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Enter value one: "));
+		
+		int y = Integer.parseInt(JOptionPane.showInputDialog("Enter value two: "));
+		
+		
+		int z = sumOfNums(x, y);
+		
+		JOptionPane.showMessageDialog(null, "" + x + " + " + y + " = " + z);
+		
+	}
+	
+	static void greeting(String name) {
+		
+		System.out.println("Hello " + name);
+		
+	}
+	
+	static int sumOfNums(int x, int y) {
+		
+		int z = x + y;
+		
+		return z;
+		
 	}
 	
 }
+
---
+++ MyApp/src/tech/chairforce/app/OverLoadedMethods.java
@@ -0,0 +1,5 @@
+package tech.chairforce.app;
+
+public class OverLoadedMethods {
+
+}
