e8a1a8961dcb497aa1ca56b98a7630d1c436534d
mosserryan <mosserryan@yahoo.com>
2022-01-20 17:11:25 -0800

    Car object being used with another java file Main.

--- MyApp/src/oop/app/Car.java
+++ MyApp/src/oop/app/Car.java
@@ -2,6 +2,18 @@ package oop.app;
 
 public class Car {
 
-	String make = "Subaru";
+	 String make = "Subaru";
+	 String model = "Forester";
+	 int year = 2016;
+	 String color = "White";
+	 double price = 30000.00;
+	
+	void drive () {
+		System.out.println("You drive the car.");
+	}
+	
+	void brake() {
+		System.out.println("You step on the brakes.");
+	}
 	
 }
--- MyApp/src/oop/app/Main.java
+++ MyApp/src/oop/app/Main.java
@@ -2,4 +2,21 @@ package oop.app;
 
 public class Main {
 
+public static void main(String[] args) {
+		
+	String make = "The make is ";
+	String model = "The model is ";
+	
+	Car myCar = new Car();
+	
+	System.out.println(make + myCar.make);
+	System.out.println(model + myCar.model);
+	
+	myCar.drive();
+	myCar.brake();
+		
+	}
+	
+	
+	
 }
