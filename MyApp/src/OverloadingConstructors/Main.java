65e31689d2a44796f7ba1d366f15b3ca574cecd9
mosserryan <mosserryan@yahoo.com>
2022-01-23 17:41:30 -0800

    Overloading pizza is completed, added some if else
    statements to spice it up with the methods.

--- MyApp/src/OverloadingConstructors/Main.java
+++ MyApp/src/OverloadingConstructors/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Pizza pizzaOne = new Pizza("mozzarella", "tomato", "pepperoni", "thin Crust");
+		Pizza pizzaTwo = new Pizza("mozzarella", "tomato", "thin Crust");
+		Pizza pizzaThree = new Pizza("tomato", "thin Crust");
+		
+		pizzaOne.prepPizza();
+		pizzaTwo.prepPizza();
+		pizzaThree.prepPizza();
+		
+		pizzaOne.cookPizza();
+		pizzaTwo.cookPizza();
+		pizzaThree.cookPizza();
+		
+		pizzaOne.isReady();
+		pizzaTwo.isReady();
+		pizzaThree.isReady();
+		
 	}
 	
 }
--- MyApp/src/OverloadingConstructors/Pizza.java
+++ MyApp/src/OverloadingConstructors/Pizza.java
@@ -2,4 +2,68 @@ package OverloadingConstructors;
 
 public class Pizza {
 
+	String cheese;
+	String sauce;
+	String toppings;
+	String crust;
+	
+	Pizza(String cheese, String sauce, String toppings, String crust) {
+		
+		this.cheese = cheese;
+		this.sauce = sauce;
+		this.toppings = toppings;
+		this.crust = crust;
+		
+	}
+	
+	Pizza(String cheese, String sauce, String crust) {
+		
+		this.cheese = cheese;
+		this.sauce = sauce;
+		this.crust = crust;
+		
+	}
+	
+	Pizza(String sauce, String crust) {
+		
+		this.sauce = sauce;
+		this.crust = crust;
+		
+	}
+	
+	void prepPizza() {
+		
+		if(this.cheese != null && this.toppings != null) {
+			System.out.println("Prepping the following ingredients: " + this.cheese + ", " + this.sauce + ", " + this.toppings + ", and " + this.crust + ".");
+		} else if (this.cheese != null && this.toppings == null) {
+			System.out.println("Prepping the following ingredients: " + this.cheese + ", " + this.sauce + ", and " + this.crust + ".");
+		} else {
+			System.out.println("Prepping the following ingredients: " + this.sauce + " and " + this.crust + ".");
+		}
+		
+		
+	}
+	
+	void cookPizza() {
+		if(this.cheese != null && this.toppings != null) {
+			System.out.println("Cooking up the " + this.cheese + " and " + this.toppings + " pizza with a " + this.sauce + " sauce and a " + this.crust + ".");
+		} else if (this.cheese != null && this.toppings == null) {
+			System.out.println("Cooking up the " + this.cheese + " pizza with " + this.sauce + " sauce and a " + this.crust + ".");
+		} else {
+			System.out.println("Cooking up the vegan pizza with " + this.sauce + " sauce and a " + this.crust + ".");
+		}
+	}
+	
+	void isReady() {
+		
+		if(this.cheese != null && this.toppings != null) {
+			System.out.println("The " + this.cheese + " and " + this.toppings + " pizza is ready!");
+		} else if (this.cheese != null && this.toppings == null) {
+			System.out.println("The " + this.cheese + " cheese pizza is ready!");
+		} else {
+			System.out.println("The vegan pizza is ready!");
+		}
+		
+	}
+	
 }
